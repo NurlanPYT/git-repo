6f0379ff385ef254277078438ab8d4592770820d
Nurlan Memmedov <nurlan.memmed0v@yandex.com>
2023-09-17 16:12:14 +0400

    Helllo

---
+++ data_type.py
@@ -0,0 +1,9 @@
+# name = "Nurlan"
+# surname = "Memmedov"
+# age = "25"
+
+# text = "My name is " + name + " and surname is " + surname
+# print(text)
+
+password = "12345678abcdefghesgft"
+print(password[-1])
---
+++ variables.py
@@ -0,0 +1,29 @@
+# 5000 + %18 
+"""
+print(7000+ (7000*0.18))
+print(8000+ (8000*0.18))
+"""
+
+# urun1 = 7000
+# urun2 = 8500
+# kdv = 0.20
+
+# print(urun1 + (urun1 * kdv))
+# print(urun2 + (urun2 * kdv))
+
+# Degiskenler sayi ile baslamaz
+
+# x = 1
+# print(type(x))
+
+
+
+name = "Nurlan"
+surname = " Məmmədov"
+print(name +  surname)
+print(type(name))
+studebt = True
+print(type(studebt))
+
+
+
